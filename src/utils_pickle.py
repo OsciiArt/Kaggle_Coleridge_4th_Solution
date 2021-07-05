04b0cb2a1f6b1f0a92d08644dfc875f46c13525e
OsciiArt <oakiyama1986@gmail.com>
2021-07-05 21:39:21 +0000

    1st commit

---
+++ src/utils_pickle.py
@@ -0,0 +1,12 @@
+import os, glob, pickle, time, gc, copy
+
+
+def pickle_save(path, df):
+    with open(path, 'wb') as f:
+        pickle.dump(df, f)
+
+
+def pickle_load(path):
+    with open(path, 'rb') as f:
+        df = pickle.load(f)
+    return df
